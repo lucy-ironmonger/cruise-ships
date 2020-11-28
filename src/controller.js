1b4f7da7ae1d59469cf5cb4a79fb3e2534c967ae
Shona McBride <mcbride.shona@gmail.com>
2020-11-28 17:44:07 +0000

    cruisy gui-ey

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,76 @@
-(function () {class Controller {
-    constructor() {
-        this.initialiseSea();
-    }
+(function () {
+    class Controller {
+        constructor(ship) {
+            this.initialiseSea();
+            this.ship = ship;
+
+            document.querySelector('#sailbutton').addEventListener('click', () => {
+                this.setSail();
+            });
+        }
+        setSail() {
+            const currentPortIndex = this.ship.itinerary.ports.indexOf(this.ship.currentPort);
+            const nextPortIndex = currentPortIndex + 1;
+            const nextPortElement = document.querySelector(`[data-port-index='${nextPortIndex}']`);
+
+            if (!nextPortElement) {
+                return alert('End of the line!');
+            }
+            this.renderMessage(`Now departing ${this.ship.currentPort.name}`);
+            const shipElement = document.querySelector('#ship');
+
+            const portPosition = nextPortElement.offsetLeft;
+            
+            let shipPosition = shipElement.offsetLeft;
+
+            ship.setSail();
+            const sailInterval = setInterval(() => {
+                if (shipPosition === portPosition - 32) {
+                    clearInterval(sailInterval);
+                }
+                shipElement.style.left = `${shipPosition + 1}px`;
+                shipPosition += 1;
+            }, 10);
+
+            ship.dock();
+            setTimeout(() => {
+                this.renderMessage(`Now arriving at ${this.ship.itinerary.ports[nextPortIndex].name}`);
+            }, 2000);
+        }
+
+        renderMessage(message) {
+            const messageElement = document.createElement('div');
+            messageElement.id = 'message';
+            messageElement.innerHTML = message;
+
+            const viewport = document.querySelector('#viewport');
+            viewport.appendChild(messageElement);
+            setTimeout(() => {
+                viewport.removeChild(messageElement);
+            }, 2000);
+        };
+
+
+
+
+
+
+    //     const shipElement = document.querySelector('#ship');
+    //     const sailInterval = setInterval(() => {
+    //         const shipLeft = parseInt(shipElement.style.left, 10);
+    //         if (shipLeft === (nextPortElement.offsetLeft - 32)) {
+    //             ship.setSail();
+    //             ship.dock();
+    //             clearInterval(sailInterval);
+    //         }
+
+    //         shipElement.style.left = `${shipLeft + 1}px`;
+    //     }, 20);
+
+    //     if (!nextPortElement) {
+    //         return alert('End of the line!');
+    //     }
+    // };
 
     initialiseSea() {
         const backgrounds = [
@@ -14,7 +83,7 @@
             backgroundIndex += 1;
         }, 1000);
     }
-    
+
     renderPorts(ports) {
         let portsElement = document.querySelector('#ports');
         portsElement.style.width = '0px';
@@ -27,21 +96,21 @@
             portsElement.appendChild(newPortElement);
             const portsElementWidth = parseInt(portsElement.style.width, 10);
             portsElement.style.width = `${portsElementWidth + 256}px`;
-          });
+        });
     }
-    renderShip(ship) {
-        const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+    renderShip() {
+        const shipPortIndex = this.ship.itinerary.ports.indexOf(this.ship.currentPort);
         const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
         const shipElement = document.querySelector('#ship');
-        shipElement.style.top = `${portElement.offsetTop + 32}px`;
+        shipElement.style.top = `${portElement.offsetTop - 60}px`;
         shipElement.style.left = `${portElement.offsetLeft - 32}px`;
     }
 
-  }
+}
 
-  if (typeof module !== 'undefined' && module.exports) {
+    if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
-  } else {
+} else {
     window.Controller = Controller;
-  }
-  }());
+}
+}());
